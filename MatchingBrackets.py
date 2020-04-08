ffff03a321f0283a5ee9d52fada17b4c8eb27fc3
Anjan kumar <anjan0103@gmail.com>
2020-04-08 13:16:58 -0400

    replaced to user input with looping options

--- MatchingBrackets.py
+++ MatchingBrackets.py
@@ -1,19 +1,23 @@
-def bracket_matching(s):
+while True:
+ def bracket_matching(s):
     d = {']': '[', ')': '(', '}': '{'}
     stack = []
     while (s):
         c = s[0]
         if (c in d and len(stack) == 0) or (c in d and stack[-1] != d[c]):
-            return False
+            return ("pair not matching")
         if c in d and stack[-1] == d[c]:
             stack.pop()
         elif c not in d:
             stack.append(c)
         s = s[1:]
     if stack:
-        return False
-    return True
+        return ("pair not matching")
+    return ("matching pairs")
+ print(bracket_matching(input("enter the brackets as many pairs as you want:")))
+ opt= (input("enter 'N' to stop: "))
+ if opt=="N":
+  break
 
 
-print(bracket_matching(input("enter the brackets:")))
 
